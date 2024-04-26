6649b71fc0f75d33b7bf6ee9c913985906a9ea6f
Nikola Matković <nikola@quaaant.com>
2024-04-26 21:50:58 +0200

    Create registration

--- hakaton/src/views/RegisterView.vue
+++ hakaton/src/views/RegisterView.vue
@@ -1,64 +1,68 @@
 <template>
     <div class="login-page">
         <div class="forma">
-            <form
-                action=""
-                class="register-form"
-                method="POST"
-                enctype="multipart/form-data"
-            >
-                <h2>DOBRODOŠLI</h2>
-                <div>
-                    <input type="text" placeholder="Ime" name="name" value="" />
-                </div>
+            <h2>DOBRODOŠLI</h2>
+            <div>
+                <input type="text" v-model="firstName" placeholder="Ime" />
+            </div>
 
-                <div>
-                    <input
-                        type="text"
-                        placeholder="Prezime"
-                        name="sname"
-                        value=""
-                    />
-                </div>
+            <div>
+                <input type="text" placeholder="Prezime" v-model="lastName" />
+            </div>
 
-                <div>
-                    <input
-                        type="email"
-                        placeholder="E-mail"
-                        name="email"
-                        value=""
-                    />
-                </div>
+            <div>
+                <input type="email" placeholder="E-mail" v-model="email" />
+            </div>
 
-                <div class="password-container">
-                    <input
-                        :type="passwordVisible ? 'password' : 'text'"
-                        placeholder="Lozinka*"
-                        name="password"
-                        autocomplete="on"
-                        class="password-input"
-                        id="password-input"
-                    />
-                    <span
-                        class="toggle-password"
-                        @click="passwordVisible = !passwordVisible"
-                    >
-                        <i class="fas fa-eye-slash" id="eye-icon"></i>
-                    </span>
-                </div>
+            <div class="password-container">
+                <input
+                    :type="passwordVisible ? 'password' : 'text'"
+                    placeholder="Lozinka*"
+                    name="password"
+                    autocomplete="on"
+                    class="password-input"
+                    id="password-input"
+                    v-model="password"
+                />
+                <span
+                    class="toggle-password"
+                    @click="passwordVisible = !passwordVisible"
+                >
+                    <i class="fas fa-eye-slash" id="eye-icon"></i>
+                </span>
+            </div>
 
-                <button class="btn" type="submit">Registrujte se</button>
-            </form>
+            <button @click="register" class="btn" type="submit">
+                Registrujte se
+            </button>
         </div>
     </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+import axios from "axios";
 
-import { ref } from 'vue';
+const passwordVisible = ref(false);
 
-const passwordVisible = ref(false)
+const email = ref("");
+const firstName = ref("");
+const lastName = ref("");
+const password = ref("");
 
+async function register() {
+    let res = await axios.post(
+        "http://hakaton-api.test/api/v1/auth/register",
+        {
+            first_name: firstName.value,
+            last_name: lastName.value,
+            email: email.value,
+            password: password.value,
+        }
+    );
+
+    console.log("res", res);
+}
 </script>
 
 <style scoped>
