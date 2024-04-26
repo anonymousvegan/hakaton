d57c0dc46d5c10001272fe17901e3187975db6df
vitohvala <hvalavitalisu@gmail.com>
2024-04-26 17:33:17 +0200

    add icons

---
+++ hakaton/src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer>
+        Copyright&#169;Tim Linux
+    </footer>
+</template>
+
+
+<style scoped> 
+    footer{
+        display: flex;
+        outline: solid red;
+        justify-content: center;
+    }
+
+</style>
