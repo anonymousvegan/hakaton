ee2657e4eabf01a84c43962416b1a023056ddfce
Nikola Matković <nikola@quaaant.com>
2024-04-27 11:41:09 +0200

    Save progress

--- hakaton/src/stores/main.js
+++ hakaton/src/stores/main.js
@@ -7,14 +7,27 @@ const useStore = defineStore("mainStore", () => {
 
     const language = ref("");
     const supportedLanguages = ["en", "sr"];
+    const progress = ref({});
+
+    function setProgressForUser(category, index){
+        progress.value[category] = index;
+    }
 
     watch(language, (newLang) => {
         localStorage.setItem("lang", newLang);
     });
 
+    watch(progress, () => {
+        localStorage.setItem("progress", JSON.stringify(progress.value))
+    }, {
+        deep: true
+    })
+
     return {
         language,
         supportedLanguages,
+        progress,
+        setProgressForUser,
     }
 
 })
